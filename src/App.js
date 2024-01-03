4671d28a68d0cb838fd91b1fd7c41abd89821ea6
NAGARJUNA2407 <nagarjunaballa01@gmail.com>
2024-01-03 20:23:11 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   <div className="flex flex-col justify-center items-center bg-gray-200 w-[100vw] h-[100vh]">
     <div className="text-center ">
       <h1 className="text-4xl font-bold">Our Testimonials </h1>
-      <div className="bg-violet-400 h-[4px] w-[1/5] mt-1"></div>
+      <div className="bg-violet-400 h-[4px] w-3/5 mt-1 mx-auto"></div>
     </div>
     <Testimonial reviews={reviews} />
 
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
+import { FaChevronLeft ,FaChevronRight } from "react-icons/fa";
 
 const Testimonial = (props) => {
   let reviews=props.reviews;
+  let [index,setIndex]=useState(0);
 
+  function leftShiftHandler(){
+    if(index-1<0){
+      setIndex(reviews.length -1 )
+    }
+    else{
+      setIndex(index-1);
+    }
+  }
+  function rightShiftHandler(){
+    if(index+1>=reviews.length){
+      setIndex(0); 
+    }
+    else{setIndex(index+1);}
+  }
+  function surpriseHandler(){
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  }
   
     return (
-    <div>
-      <Card review={reviews[0]} />
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center hover:shadow-xl mt-10 p-10 transition-all duration-700 rounded-md'>
+      <Card review={reviews[index]} />
+      
+      <div className='flex text-3xl mt-9 gap-3 text-violet-400 font-bold mx-auto '>
+      <button className='cursor-pointer hover:text-violet-500' onClick={leftShiftHandler}><FaChevronLeft /></button>
+      <button className='cursor-pointer hover:text-violet-500' onClick={rightShiftHandler}><FaChevronRight /></button> 
+      </div> 
+
+      <div className='mx-auto mt-6'>
+        <button className='bg-violet-500 hover:bg-violet-600 transition-all duration-200 px-10 py-2 cursor-pointer rounded-md font-bold text-white text-lg ' onClick={surpriseHandler}>Surprise Me</button>
+      </div>
     </div>
   )
 }
